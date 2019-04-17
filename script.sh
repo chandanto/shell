c9ef1609f6d987cfa8b987dcdb4b990847e4507a
chandanto <chandanram.to@gmail.com>
2019-04-17 09:20:00 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,3 +2,13 @@
 
 yum install -y docker 
 for i in docker; do for n in start status; do systemctl $n $i; done;done
+
+docker swarm init
+docker swarm join-token worker  | grep -v command > /tmp/worker.sh
+chmod 740 /tmp/worker.sh
+
+wget https://raw.githubusercontent.com/chandanto/ppk/master/chandan_public_key_pem
+chmod 640 chandan_public_key_pem
+
+scp -o StrictHostKeyChecking=no -i  'chandan_public_key_pem' /tmp/worker.sh root@vm-2:/tmp
+scp -o StrictHostKeyChecking=no -i  'chandan_public_key_pem' /tmp/worker.sh root@vm-3:/tmp
