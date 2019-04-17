afeb9bb19bcd0510070626bd8a5043a1e68b6bcd
chandanto <chandanram.to@gmail.com>
2019-04-17 11:30:37 +0100

    Update script.sh
    All build preparation part completed ...

--- script.sh
+++ script.sh
@@ -7,15 +7,15 @@ docker swarm init
 docker swarm join-token worker  | grep -v command > /tmp/worker.sh
 chmod 740 /tmp/worker.sh
 
-cd /tmp
-wget https://raw.githubusercontent.com/chandanto/ppk/master/chandan_public_key_pem
-chmod 600 chandan_public_key_pem
+cd /tmp; wget https://raw.githubusercontent.com/chandanto/ppk/master/chandan_public_key_pem; chmod 600 chandan_public_key_pem
 
 scp -o StrictHostKeyChecking=no -i  'chandan_public_key_pem' /tmp/worker.sh chandan@vm-2:/tmp
 scp -o StrictHostKeyChecking=no -i  'chandan_public_key_pem' /tmp/worker.sh chandan@vm-3:/tmp
 
-rm -rf /tmp/chandan_public_key_pem
-rm -rf /tmp/worker.sh
+rm -rf /tmp/chandan_public_key_pem; rm -rf /tmp/worker.sh
 
+cd /opt; wget https://github.com/chandanto/forward-files/raw/master/updated-code_docker_compose.tar
+tar -xvf updated-code_docker_compose.tar
+rm -rf updated-code_docker_compose.tar
 
 
