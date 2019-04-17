321646d2755a6906ec02f6c7a6c8f0f861d312c6
chandanto <chandanram.to@gmail.com>
2019-04-17 10:06:44 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ docker swarm join-token worker  | grep -v command > /tmp/worker.sh
 chmod 740 /tmp/worker.sh
 
 wget https://raw.githubusercontent.com/chandanto/ppk/master/chandan_public_key_pem
-chmod 640 chandan_public_key_pem
+chmod 600 chandan_public_key_pem
 
 scp -o StrictHostKeyChecking=no -i  'chandan_public_key_pem' /tmp/worker.sh root@vm-2:/tmp
 scp -o StrictHostKeyChecking=no -i  'chandan_public_key_pem' /tmp/worker.sh root@vm-3:/tmp
